980f9163cea27cb0df8c56701059ba07f296f7ff
Reginaldo Miranda <reginaldobrain@gmail.com>
2025-07-19 11:51:06 -0300

    versao boa

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 import express, { json } from "express";
 import { PrismaClient } from "@prisma/client";
-import cors from "cors"
+import cors from "cors";
 
 const prisma = new PrismaClient();
 
@@ -8,8 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors()) // <-- Executar a função
-
+app.use(cors()); // <-- Executar a função
 
 app.post("/usuarios", async (req, res) => {
   await prisma.user.create({
@@ -44,19 +43,18 @@ app.get("/usuarios", async (req, res) => {
 app.put("/usuarios/:id", async (req, res) => {
   // editar um registro
 
- await prisma.user.update({
-  where:{
-    id:req.params.id
-  },
-  data:{
-  name: req.body.name,
-  email: req.body.email,     
-  age: req.body.age
-    }
+  await prisma.user.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      name: req.body.name,
+      email: req.body.email,
+      age: req.body.age,
+    },
   });
   res.status(201).json(req.body);
 });
-
 
 app.delete("/usuarios/:id", async (req, res) => {
   // deletar um registro
@@ -66,6 +64,75 @@ app.delete("/usuarios/:id", async (req, res) => {
     },
   });
   res.status(200).json({ message: "usuário deletado com sucesso" });
+});
+
+
+
+
+
+
+
+
+//-------------------------- veiculos -----------------------------
+
+app.post("/Veiculos", async (req, res) => {
+  await prisma.veiculos.create({
+    data: {
+      placa: req.body.placa,
+      marca: req.body.marca,
+      cor: req.body.cor,
+    },
+  });
+  res.status(201).json(req.body);
+});
+
+app.get("/Veiculos", async (req, res) => {
+  // lista todos
+
+  let users = [];
+
+  if (req.query) {
+    users = await prisma.veiculos.findMany({
+      where: {
+        marca: req.query.marca,
+        modelo: req.query.modelo,
+        ano: req.query.ano,
+        cor: req.query.cor,
+        placa: req.query.placa,
+      },
+    });
+  } else {
+    users = await prisma.veiculos.findMany();
+  }
+  res.status(200).json(users);
+});
+
+app.put("/Veiculos/:id", async (req, res) => {
+  // editar um registro
+
+  await prisma.veiculos.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      marca: req.body.marca,
+      modelo: req.body.modelo,
+      ano: req.body.ano,
+      cor: req.body.cor,
+      placa: req.body.placa,
+    },
+  });
+  res.status(201).json(req.body);
+});
+
+app.delete("/Veiculos/:id", async (req, res) => {
+  // deletar um registro
+  await prisma.veiculos.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.status(200).json({ message: "Veiculo deletado com sucesso" });
 });
 
 app.listen(3000);
---
+++ parada/src/pages/Home/index.jsx
@@ -0,0 +1,79 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState, useRef } from "react";
+import "./style.css";
+import Lixeira from "../../assets/react.svg";
+import api from "../../services/api";
+
+function Home() {
+  const [carro, setCarro] = useState([]);
+
+  const inputPlaca = useRef();
+  const inputMarca = useRef();
+  const inputCor= useRef();
+
+ /* const [carro, setCarro] = useState([
+   { id: 1, placa: "Teste", marca: 99, cor: "teste@email.com" }
+  ]);*/
+
+  async function getUsers() {
+    const userFromApi = await api.get("/Veiculos");
+    setCarro(userFromApi.data);
+  }
+
+  async function createUser() {
+    await api.post("/Veiculos", {
+      placa: inputPlaca.current.value,
+      marca: inputMarca.current.value,
+      cor: inputCor.current.value
+    });
+    getUsers();
+  }
+
+  async function deleteUser(id) {
+    await api.delete(`/Veiculos/${id}`);
+    getUsers(); // Atualiza a lista após a exclusão
+  }
+
+
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <div className="container">
+      <form>
+        <h1>Cadastro de Veiculos</h1>
+        <input name="placa" type="text" placeholder="placa" ref={inputPlaca} />
+        <input name="marca" type="text" placeholder="marca" ref={inputMarca} />
+        <input name="cor" type="text" placeholder="cor" ref={inputCor} />
+        <button type="button" onClick={createUser}>
+          Cadastrar
+        </button>
+      </form>
+     
+      {carro.map((Veiculos) => (
+        <div key={Veiculos.id} className="card">
+          <div>
+            <p>
+              Placas : <span>{Veiculos.placa}</span>
+            </p>
+            <p>
+              Marca : <span>{Veiculos.marca}</span>
+            </p>
+            <p>
+              Cor :<span>{Veiculos.cor}</span>
+            </p>
+          </div>
+          <button onClick={() => deleteUser(Veiculos.id)}>
+            <img src={Lixeira} />
+          </button>
+        </div>
+        
+      ))}
+      </div>
+    
+  );
+}
+
+export default Home;
