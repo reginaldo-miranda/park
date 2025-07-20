0879e4d5a715ec05611b03803e08d5eab42a3a29
Reginaldo Miranda <reginaldobrain@gmail.com>
2025-07-20 17:25:00 -0300

    colocado mensagem

--- parada/src/pages/Home/index.jsx
+++ parada/src/pages/Home/index.jsx
@@ -4,7 +4,11 @@ import "./style.css";
 import Lixeira from "../../assets/react.svg";
 import api from "../../services/api";
 
+
+
+
 function Home() {
+  const [mensagem, setMensagem] = useState("");
   const [carro, setCarro] = useState([]);
 
   const inputPlaca = useRef();
@@ -19,7 +23,7 @@ function Home() {
     const userFromApi = await api.get("/veiculos");
     setCarro(userFromApi.data);
   }
-
+/*
   async function createUser() {
     await api.post("/veiculos", {
       placa: inputPlaca.current.value.toUpperCase(),
@@ -31,12 +35,48 @@ function Home() {
         inputMarca.current.value = "";
        inputCor.current.value = "";
     getUsers();
-  }
+  }*/
+async function createUser() {
+  await api.post("/veiculos", {
+    placa: inputPlaca.current.value.toUpperCase(),
+    marca: inputMarca.current.value.toUpperCase(),
+    cor: inputCor.current.value.toUpperCase()
+  });
 
+  // Limpar os campos
+  inputPlaca.current.value = "";
+  inputMarca.current.value = "";
+  inputCor.current.value = "";
+
+  // Atualizar lista
+  getUsers();
+
+  // Mostrar mensagem temporária
+  setMensagem("Veículo cadastrado com sucesso!");
+
+  // Esconde a mensagem depois de 3 segundos
+  setTimeout(() => setMensagem(""), 3000);
+}
+
+
+
+/*
   async function deleteUser(id) {
     await api.delete(`/veiculos/${id}`);
     getUsers(); // Atualiza a lista após a exclusão
   }
+*/
+
+async function deleteUser(id) {
+  const confirma = window.confirm("Tem certeza que quer deletar este veículo?");
+  if (!confirma) return; // cancela se não confirmou
+
+  await api.delete(`/veiculos/${id}`);
+  getUsers();
+
+  setMensagem("Veículo deletado com sucesso!");
+  setTimeout(() => setMensagem(""), 3000);
+}
 
 
 
@@ -45,9 +85,12 @@ function Home() {
   }, []);
 
   return (
+    
+
     <div className="container">
   <div className="content">
-    <form className="form">
+    
+    <form className="form" >
       <h1>Cadastro de Veículos</h1>
       <input name="placa" type="text" placeholder="Placa" ref={inputPlaca} />
       <input name="marca" type="text" placeholder="Marca" ref={inputMarca} />
@@ -55,6 +98,7 @@ function Home() {
       <button type="button" onClick={createUser}>
         Cadastrar
       </button>
+      {mensagem && <div className="mensagem-sucesso">{mensagem}</div>}
     </form>
 
     <div className="cards">
